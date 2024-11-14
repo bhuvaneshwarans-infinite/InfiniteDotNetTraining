6d0be94e1479f47ada66e7ffc1a3d66b42249a47
Bhuvaneshwaran Saravanan <bhuvaneshwarans@infinite.com>
2024-11-14 20:46:54 +0530

    Assignment 5

---
+++ C# Assignment/Assignment 5/Question2.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.IO;
+
+//Write a program in C# Sharp to create a file and write an array of strings to the file.
+
+namespace Assignment5
+{
+    class Question2
+    {
+        static void Main()
+        {
+            try
+            {
+                    Console.Write("How many Lines your poetry contain:");
+                    int linesCount = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("\nEnter your Lines Below:\n");
+                    string[] lines = new string[linesCount];
+                    for (int i = 0; i < linesCount; i++)
+                    {
+                        Console.Write($"Line {i + 1}:");
+                    //lines[i] = Console.ReadLine() + "\n";
+                    lines[i] = Console.ReadLine();
+                    }
+                string filePath = "poetry.txt";
+
+                using (StreamWriter writer = new StreamWriter(filePath))
+                {
+                    foreach (string line in lines)
+                    {
+                        writer.WriteLine(line);
+                    }
+                }
+                Console.WriteLine("File Created and data is written to the file.");
+            }
+            catch (UnauthorizedAccessException uae)
+            {
+                Console.WriteLine("You don't have an access to create a file in particular folder");
+            }
+            catch (FormatException fe)
+            {
+                Console.WriteLine("Please enter proper data");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Some exception didn't handled");
+            }
+            Console.WriteLine("\n\n***Press Enter to exit***");
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ C# Assignment/Assignment 5/Question3.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.IO;
+
+
+namespace Assignment5
+{
+    class Question3
+    {
+        static void Main()
+        {
+
+            try
+            {
+		string filePath = "poetry.txt";
+            	int lineCount = 0;
+
+                if (File.Exists(filePath))
+                {
+                    using (StreamReader reader = new StreamReader(filePath))
+                    {
+                        while (reader.ReadLine() != null)
+                        {
+                            lineCount++;
+                        }
+                    }
+                    Console.WriteLine("===========Result===========\n");
+                    Console.WriteLine($"No of lines we can find in your file is {lineCount}");
+                }
+                else
+                {
+                    throw new FileNotFoundException("Please check your file name and file path.Their is no file exists.");
+
+                }
+            }
+            catch (FileNotFoundException  ffe)
+            {
+                Console.WriteLine(ffe.Message);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("An error occurred: " + ex.Message);
+            }
+            Console.WriteLine("\n\n***Press Enter to exit***");
+            Console.ReadKey();            
+        }
+    }
+}
