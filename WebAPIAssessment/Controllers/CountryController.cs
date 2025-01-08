fa37965e855dfd720ee9d1654b79876a183f7ddd
Bhuvaneshwaran Saravanan <bhuvaneshwarans@infinite.com>
2025-01-08 10:39:30 +0530

    Web API Assessment

---
+++ WebAPIAssessment/Controllers/CountryController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web.Http;
+using WebApiAssessment.Models;
+
+namespace WebApiAssessment.Controllers
+{
+    public class CountryController : ApiController
+    {
+        public static List<CountryMdl> countries = new List<CountryMdl>();
+        
+        [Route("api/Country")]
+        public IEnumerable<CountryMdl> GetCountries()
+        {
+            return countries;
+        }
+
+        [HttpPost]
+        [Route("api/Country/Create")]
+        public List<CountryMdl> CreateCountry(CountryMdl country)
+        {
+            countries.Add(country);
+            return countries;
+        }
+ 
+
+        [HttpPut]
+        [Route("api/Country/Update")]
+        public IEnumerable<CountryMdl> UpdateCountry(int Cid,string country,string captial)
+        {
+            CountryMdl UpdateCOuntryObj = new CountryMdl();
+            UpdateCOuntryObj.ID = Cid;
+            UpdateCOuntryObj.CountryName = country;
+            UpdateCOuntryObj.Capital = captial;
+            countries[Cid - 1] = UpdateCOuntryObj;
+            return countries;
+        }
+
+        [HttpDelete]
+        [Route("api/Country/Delete")]
+        public IEnumerable<CountryMdl> Delete(int Cid)
+        {
+            countries.RemoveAt(Cid - 1);
+            return countries;
+        }
+
+    }
+}
---
+++ WebAPIAssessment/Models/CountryMdl.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApiAssessment.Models
+{
+    public class CountryMdl
+    {
+        public int ID { get; set; }
+        public string CountryName { get; set; }
+        public string Capital { get; set; }
+    }
+}
